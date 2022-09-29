e25ceb68597130647210de3a1ba30356ac681981
siliziwe <siliziwepani@gmail.com>
2022-09-29 09:42:53 +0100

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Sli's Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quae quod odio, distinctio rerum veritatis atque cupiditate labore sapiente odit aliquid magni ullam, aspernatur velit beatae tenetur ab? Blanditiis, optio!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quae quod odio, distinctio rerum veritatis atque cupiditate labore sapiente odit aliquid magni ullam, aspernatur velit beatae tenetur ab? Blanditiis, optio!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quae quod odio, distinctio rerum veritatis atque cupiditate labore sapiente odit aliquid magni ullam, aspernatur velit beatae tenetur ab? Blanditiis, optio!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quae quod odio, distinctio rerum veritatis atque cupiditate labore sapiente odit aliquid magni ullam, aspernatur velit beatae tenetur ab? Blanditiis, optio!</p>
+  </div>
+</template>
